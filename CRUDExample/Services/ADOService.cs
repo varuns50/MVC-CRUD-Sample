a9e1ea0ad08fe229f293f09a3bd401a328150b18
vikas.suthar <vikas.suthar@adapty.com>
2018-10-26 10:57:45 +0530

    Add project files.

---
+++ CRUDExample/Services/ADOService.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessUtility;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUDExample.Services
+{
+    public class ADOService
+    {
+
+        public void ExecuteQuery()
+        {
+            // add a customer
+            Dictionary<string, SqlParameter> cmdParameters = new Dictionary<string, SqlParameter>();
+            cmdParameters["name"] = new SqlParameter("name", "Smith");
+            cmdParameters["state"] = new SqlParameter("state", "MD");
+            SqlDatabaseUtility.ExecuteCommand("mssqltips", "dbo.AddCustomer", cmdParameters);
+
+            // query the customer table
+            Dictionary<string, SqlParameter> queryParameters = new Dictionary<string, SqlParameter>();
+            SqlDataReader reader = SqlDatabaseUtility.ExecuteQuery(
+                "mssqltips", "dbo.GetCustomerList", queryParameters);
+            while (reader.Read() == true)
+            {
+                Console.WriteLine(string.Format("{0}\t{1}\t{2}",
+                    reader[0].ToString(),
+                    reader[1].ToString(),
+                    reader[2].ToString()));
+            }
+        }
+}
+}
